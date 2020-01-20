0a8fbf3ba95b43272cac640706f933c64425907e
Lukas Ra <lukas.razukevicius@gmail.com>
2020-01-20 11:03:46 +0200

    update

---
+++ inc/links.php
@@ -0,0 +1,16 @@
+<?php
+
+$navigacija = [
+        'sidebar' => [
+            'naujausi' => 'linkas',
+            'visi filmai' => 'linkas',
+            'paieska' => 'linkas',
+            'redaguoti' => 'linkas'
+        ],
+        'dropdown' => [
+            'prisijungti' => 'linkas',
+            'ideti filma' => 'linkas',
+            'redaguoti' => 'linkas',
+            'trinti filma' => 'linkas'
+        ]
+];
